c55547a49baf3245eacdc9c05699ac278f298cf1
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-11-29 14:04:02 +0530

    Deployment

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import RootLayout from "./layout"
 import LandingPage from "./Section/LandingPage"
 import SignupForm from "./components/SignupPage"
-import { Navigate, Route, Routes } from "react-router-dom"
+import {  Route, Routes } from "react-router-dom"
 import LoginForm from "./components/SigninPage"
 
 import AddSchool from "./Section/School/add-school"
@@ -11,13 +11,7 @@ import ViewSchools from "./Section/School/view-schools"
 import ViewStudents from "./Section/School/view-student"
 import ViewTeachers from "./Section/School/view-teachers"
 
-const isAuthorized = () => {
-  const token = localStorage.getItem("token")
-  if (token) {
-    return true
-  }
-  return false
-}
+
 export default function App() {
   return (
     <div className="min-h-screen bg-white text-gray-800">
@@ -27,12 +21,12 @@ export default function App() {
           <Route path="/signup" element={<SignupForm/>} />
           <Route path="/signin" element={<LoginForm />} />
           
-          <Route path="/addschool" element={isAuthorized() ?  <AddSchool/>:<Navigate to='/'/>  } />
-          <Route path="/addteacher" element={isAuthorized() ? <AddTeacher/>:<Navigate to='/'/> } />
-          <Route path="/addstudent" element={isAuthorized() ? <AddStudent/>:<Navigate to='/'/> } />
-          <Route path="viewschool" element={isAuthorized() ? <ViewSchools/>:<Navigate to='/'/> } />
-          <Route path="/viewteacher" element={isAuthorized() ? <ViewTeachers/>:<Navigate to='/'/> } />
-          <Route path="/view" element={isAuthorized() ? <ViewStudents/>:<Navigate to='/'/> } />
+          <Route path="/addschool" element={  <AddSchool/>  } />
+          <Route path="/addteacher" element={ <AddTeacher/> } />
+          <Route path="/addstudent" element={ <AddStudent/> } />
+          <Route path="viewschool" element={ <ViewSchools/> } />
+          <Route path="/viewteacher" element={ <ViewTeachers/> } />
+          <Route path="/view" element={ <ViewStudents/> } />
       </Routes>
       </RootLayout>
     </div>
