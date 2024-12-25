86b45af002b24ec6d3fc46b8889b4d4cdc8ae164
blockchainstar12 <dumitrasalexandru2016@gmail.com>
2024-12-25 14:10:28 +0530

    update

--- Backend/services/nodemailer.js
+++ Backend/services/nodemailer.js
@@ -1,9 +1,10 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import { generateCouponImage } from '../utils/generateImage.js';
 
 dotenv.config();
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", 
+    host: "smtp.gmail.com", // e.g., "smtp.gmail.com"
     port: 587, // Standard SMTP port
     secure: false, // Use TLS (false for STARTTLS, true for SSL)
     auth: {
@@ -12,14 +13,32 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-export const sendEmail = async (to, subject, text, html) => {
+// noOfTokens,
+// student,
+// teacher,
+// subject,
+// date,
+// schoolLogoURL,
+// schoolName,
+// teacherEmail,
+// parentEmail,
+
+export const sendEmail = async (to, subject, text, html, attachment) => {
     try {
+        const imageBuffer = await generateCouponImage(97, "Mayank", "Yash","Englis","12/12/12","https://placehold.co/400x400.png","EPR RAP SCHOOL","yashvardhan@gmail.com","mandar@gmail.com");
         const info = await transporter.sendMail({
             from: process.env.EMAIL_USER,
             to,
             subject,
             text,
             html,
+            attachments: [
+                {
+                  filename: 'coupon.png',
+                  content: attachment,
+                  cid: 'couponImage', // Content-ID for referencing in the email body
+                },
+              ],
         });
         console.log('Email sent successfully', info);
         return true
@@ -28,4 +47,3 @@ export const sendEmail = async (to, subject, text, html) => {
         return false
     }
 }
-
