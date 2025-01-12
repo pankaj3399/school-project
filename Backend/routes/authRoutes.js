0d4c48b9ee6e9e67c3e541bb21e30f05f3bb12a6
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-12 23:29:51 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { login,signup } from '../controllers/authController.js';
+import { login,resetPassword,sendOtp,signup, verifyOtp } from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
+router.post('/sendotp', sendOtp);
+router.post('/verify', verifyOtp);
+router.post('/resetpassword', resetPassword);
 
 export default router;
  
