315fdb4cadd1986bdf326023dd3ab4766c3829e4
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-14 15:46:56 +0530

    Update formRoutes.js

--- Backend/routes/formRoutes.js
+++ Backend/routes/formRoutes.js
@@ -2,13 +2,14 @@ import { authenticate } from "../middlewares/authMiddleware.js";
 import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 import express from 'express';
 import {Role} from '../enum.js';
-import { getFormById, getForms, getPointHistory, submitFormTeacher } from "../controllers/formController.js";
+import { getFormById, getForms, getPointHistory, submitFormAdmin, submitFormTeacher } from "../controllers/formController.js";
 
 const router = express.Router();
 
 router.get('/getForms',authenticate,authorizeRoles(Role.SchoolAdmin,Role.Teacher, Role.Student),getForms)
 router.get('/getFormById/:id',authenticate,authorizeRoles(Role.SchoolAdmin,Role.Teacher, Role.Student),getFormById)
 router.post('/submitFormTeacher/:formId',authenticate,authorizeRoles(Role.Teacher),submitFormTeacher)
+router.post('/submitFormAdmin/:formId',authenticate,authorizeRoles(Role.SchoolAdmin),submitFormAdmin)
 router.get('/getPointHistory',authenticate,authorizeRoles(Role.SchoolAdmin,Role.Teacher),getPointHistory)
 
 export default router;
