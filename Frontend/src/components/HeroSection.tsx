94284c3b239c9c793bc834897862e302be773114
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-15 13:17:58 +0530

    Update HeroSection.tsx

--- Frontend/src/components/HeroSection.tsx
+++ Frontend/src/components/HeroSection.tsx
@@ -3,22 +3,18 @@ import { Link } from 'react-router-dom';
 
 export default function HeroSection() {
   return (
-    <section id="about" className="relative bg-[#eef1f1] text-white md:py-96 mt-32 h-[80vh] ">
+    <section id="about" className="relative bg-[#eef1f1] text-white mt-40 h-[80vh] ">
       
       <div className="absolute inset-0 z-0 opacity-85 h-full bg-sky-300/40">
-        <video
-          src='/FRAMEWORK.mp4'
-          autoPlay
-          loop
-          controls={false}
-          muted
-          className='relative w-full mx-auto object-contain z-10 '
+        <img
+          src='/FRAMEWORK.gif'
+          className='relative h-full mx-auto object-contain z-10 '
         />
        
       </div>
 
     
-      <div className="container bg-transparent mx-auto px-4 flex flex-col items-center text-center justify-end pb-52 md:justify-end md:pb-0 h-full relative md:-bottom-32 z-20">
+      <div className="container bg-transparent mx-auto px-4 flex flex-col items-center text-center justify-end pb-52 md:justify-end md:pb-0 h-full relative md:bottom-48 z-20">
         <Link to='/signup'>
         <Button size="lg" className="bg-green-500 hover:bg-green-600 text-white">
           Join Now
