5f5820ff64895190401293e923ec600812123e21
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-15 13:15:48 +0530

    Update FeatureSection.tsx

--- Frontend/src/components/FeatureSection.tsx
+++ Frontend/src/components/FeatureSection.tsx
@@ -31,7 +31,7 @@ const features = [
 
 export default function FeaturesSection() {
   return (
-    <section id="features" className="py-4 mt-32 bg-gray-50">
+    <section id="features" className="py-4  bg-gray-50">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">Key Features</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
