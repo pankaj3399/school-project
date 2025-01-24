3a946cd2208a6f84567f6a1a01a5ac0edbf873d5
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-24 13:26:35 +0530

    Update dashboard.tsx

--- Frontend/src/Section/School/dashboard.tsx
+++ Frontend/src/Section/School/dashboard.tsx
@@ -1,15 +1,17 @@
 //School/dashboard.tsx
-import ByStudentStats from './component/ByStudentStats'
-import ByTeacherStats from './component/ByTeacherStats'
-import OverallStats from './component/OverallStats'
+import CurrentWeekCharts from './component/current-week-charts'
+import EducationYearChart from './component/new-chart'
+import Ranks from './component/ranks'
 
 const AdminDashboard = () => {
     
   return (
-    <div className='space-y-4'>
-      <OverallStats />
-      <ByTeacherStats />
-      <ByStudentStats />
+    <div className='space-y-4 grid grid-cols-4'>
+      <div className='col-span-3'>
+        <EducationYearChart />
+        <CurrentWeekCharts />
+      </div>
+      <Ranks/>
     </div>
   )
 }
