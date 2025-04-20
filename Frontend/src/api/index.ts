464050f38e510083d0a1ddd80102d807f74b650d
YashvardhanDhondge <yashvardhandhondge@gmail.com>
2025-04-20 13:32:58 +0530

    deploy test 1.4 - backend routing

--- Frontend/src/api/index.ts
+++ Frontend/src/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AnswerType, AnswerTypeArray } from "@/lib/types";
 
 const API_URL= import.meta.env.PROD ? "/api" : import.meta.env.VITE_API_URL
-console.log(API_URL);
+console.log(import.meta.env.PROD, import.meta.env.MODE, API_URL);
 
 
 const getToken = () => localStorage.getItem('token')
